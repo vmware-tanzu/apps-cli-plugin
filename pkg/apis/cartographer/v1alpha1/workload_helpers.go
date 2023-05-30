3cedc297c6eacc816f810980fa01f6dcf41a3408
Diego LAA <odinnordico@users.noreply.github.com>
2023-05-30 10:30:23 -0500

    fix: Update subpath from DeepCopy instead of pointer (#580)
    * chore: Add test for source updated
    
    Signed-off-by: Diego Alfonso <dalfonso@vmware.com>
    
    * chore: Remove extra empty subpath validation on MergeSourceImage
    
    Signed-off-by: Diego Alfonso <dalfonso@vmware.com>
    
    * fix: Update subpath only if provided when merging
    
    Signed-off-by: Diego Alfonso <dalfonso@vmware.com>
    
    * chore: Ignore subpath when an image is used
    
    Signed-off-by: Diego Alfonso <dalfonso@vmware.com>
    
    ---------
    
    Signed-off-by: Diego Alfonso <dalfonso@vmware.com>

--- pkg/apis/cartographer/v1alpha1/workload_helpers.go
+++ pkg/apis/cartographer/v1alpha1/workload_helpers.go
@@ -219,17 +219,28 @@ func (w *WorkloadSpec) Merge(updates *WorkloadSpec) {
 	for _, p := range updates.Params {
 		w.MergeParams(p.Name, p.Value)
 	}
+	sp := ""
+	if w.Source != nil {
+		sp = w.Source.Subpath
+	}
 	if updates.Image != "" {
 		w.MergeImage(updates.Image)
 	}
-	if s := updates.Source; s != nil {
+	if updates.Source != nil && (updates.Source.Git != nil || updates.Source.Image != "") && sp != "" {
+		if w.Source == nil {
+			w.Source = &Source{}
+		}
+		w.Source.Subpath = sp
+	}
+	if updates.Source != nil {
+		s := updates.Source.DeepCopy()
 		if s.Git != nil {
 			w.MergeGit(*s.Git)
 		}
 		if s.Image != "" {
 			w.MergeSourceImage(s.Image)
 		}
-		if s.Subpath != "" {
+		if s.Subpath != "" && w.Source != nil && (w.Source.Git != nil || w.Source.Image != "") {
 			w.MergeSubPath(s.Subpath)
 		}
 	}
@@ -326,7 +337,7 @@ func (w *WorkloadSpec) ResetSource() {
 }
 
 func (w *WorkloadSpec) MergeGit(git GitSource) {
-	stash := w.Source
+	stash := w.Source.DeepCopy()
 	image := w.Image
 	w.ResetSource()
 
@@ -341,35 +352,30 @@ func (w *WorkloadSpec) MergeGit(git GitSource) {
 		w.Source = &Source{
 			Git: &git,
 		}
-		if stash != nil && stash.Git != nil {
+		if stash != nil && stash.Subpath != "" {
 			w.Source.Subpath = stash.Subpath
 		}
 	}
 }
 
 func (w *WorkloadSpec) MergeSourceImage(image string) {
-	stash := w.Source
+	src := &Source{Image: image}
+	if w.Source != nil {
+		src.Subpath = w.Source.Subpath
+	}
 	w.ResetSource()
-
-	w.Source = &Source{
-		Image: image,
-	}
-	if stash != nil && stash.Image != "" {
-		w.Source.Subpath = stash.Subpath
-	}
+	w.Source = src
 }
 
 func (w *WorkloadSpec) MergeSubPath(subPath string) {
 	if w.Source == nil {
 		w.Source = &Source{}
 	}
-
 	w.Source.Subpath = subPath
 }
 
 func (w *WorkloadSpec) MergeImage(image string) {
 	w.ResetSource()
-
 	w.Image = image
 }
 
